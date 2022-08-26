cb97ccb2ad7f70206bae1af2849b3680e37727c6
Ignacio Debórtoli <97858812+nanodebortoli@users.noreply.github.com>
2022-08-26 18:27:53 -0300

    2608 commit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,8 +6,8 @@ const Projects = () => {
   <div className={styles.projects} id='projects'>
     <h1>Projects</h1>
     <div className={styles.container}>
-      <Project image='https://i.stack.imgur.com/Lv9Nd.png' name='Soundwave' description="Soundwave is a music streaming platform that I developed together with 7 of my bootcamp's classmates. The page consumes data from deezer API, which is stored in our own database, but also has the feature of uploading your own music if you're an artist. It was a huge challenge where I learned how to work alongside my partners and to use some new technologies like audio streaming libraries." deploy='https://soundwave-swart.vercel.app/' repo='https://github.com/SoundWavePF' />
-      <Project image='https://i.stack.imgur.com/Lv9Nd.png' name='PI Countries' description='In this project I created a webpage that consumes data from restcountries.com API. Various pieces of information about the different countries can be seen. Countries can be filtered or ordered by different criteria.' deploy='https://picountries-nanodebortoli.vercel.app/' repo='https://github.com/nanodebortoli/PICountries' />  
+      <Project image={process.env.PUBLIC_URL + '/soundwave.png'} name='Soundwave' description="Soundwave is a music streaming platform that I developed together with 7 of my bootcamp's classmates. The page consumes data from deezer API, which is stored in our own database, but also has the feature of uploading your own music if you're an artist. It was a huge challenge where I learned how to work alongside my partners and to use some new technologies like audio streaming libraries." deploy='https://soundwave-swart.vercel.app/' repo='https://github.com/SoundWavePF' />
+      <Project image={process.env.PUBLIC_URL + '/countries.png'} name='PI Countries' description='In this project I created a webpage that consumes data from restcountries.com API. Various pieces of information about the different countries can be seen. Countries can be filtered or ordered by different criteria.' deploy='https://picountries-nanodebortoli.vercel.app/' repo='https://github.com/nanodebortoli/PICountries' />  
     </div>
   </div>
   );

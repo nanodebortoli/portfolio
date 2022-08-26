95dc1570ccab6d1eae2a42580f2ef108da7b4652
Ignacio Debórtoli <97858812+nanodebortoli@users.noreply.github.com>
2022-08-26 18:35:09 -0300

    second commit 2608

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -5,7 +5,7 @@ const Welcome = () => {
     <div className={styles.welcome}>
       <h1>Welcome to my portfolio</h1>
       <div>
-        <span>I'm Ignacio Debórtoli and I'm a Full Stack Web Developer</span>
+        <span>Hi! I'm Ignacio Debórtoli and I'm a Full Stack Web Developer</span>
         <a href='https://1drv.ms/b/s!AqzScWXjraYnbi7xFUREDN-OjB4?e=b0223C' target='_blank'>Download my resume</a>
       </div>
     </div>
